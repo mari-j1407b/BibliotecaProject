55212859a69a81b280dbc4e97fb40c0733539cba
mari-j1407b <mariabelenmg1809@gmail.com>
2026-09-05 23:49:31 -0600

    Pulir estructura nodo b+

--- DataStructures/NodoBPlus.cs
+++ DataStructures/NodoBPlus.cs
@@ -1,3 +1,10 @@
+// Nodos para el arbol B+
+// Orden 4
+// max claves = m-1 = 3
+// max hijos = m = 4
+// min claves = (m/2)-1 = 1
+// min hijos = m/2 = 2
+
 using System;
 
 class Libro
@@ -9,17 +16,29 @@ class Libro
 class NodoBPlus
 {
     public bool EsHoja { get; set; }
+    public int NumClaves { get; set; }
     public Libro[] Claves { get; set; }
+    public NodoBPlus[] Hijos { get; set; }
+    public NodoBPlus Siguiente { get; set; }
+
+    public NodoBPlus(bool esHoja)
+    {
+        EsHoja = esHoja;
+        NumClaves = 0;
+        Claves = new Libro[3];
+        Hijos = new NodoBPlus[4];
+        Siguiente = null;
+    }
 }
 
 class Program
 {
     static void Main()
     {
-        NodoBPlus nodo = new NodoBPlus();
+        NodoBPlus hoja = new NodoBPlus(true);
+        NodoBPlus interno = new NodoBPlus(false);
 
-//        Console.WriteLine("Nodo creado");
-
-        Console.WriteLine(nodo.Claves.Length);
+        Console.WriteLine("Hoja: " + hoja.EsHoja);
+        Console.WriteLine("Interno: " + interno.EsHoja);
     }
 }
