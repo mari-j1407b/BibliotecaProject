d79c749b1ec560dd51b0d09afa1b7e62c49a0c07
mari-j1407b <mariabelenmg1809@gmail.com>
2026-09-05 20:34:02 -0600

    estructuras MaxHeap y MinHeap para hacer reporte de libros mas y menos prestados

---
+++ DataStructures/MaxHeap.cs
@@ -0,0 +1,129 @@
+//  Max heap
+
+using System;
+
+class MaxHeap
+{
+    private Libro[] arreglo;
+    private int tamanio;
+    public MaxHeap(int capacidad)
+    {
+        arreglo = new Libro[capacidad];
+        tamanio = 0;
+    }
+
+
+
+    // Insertar nuevo libro
+    public void Insertar(Libro libro)
+    {
+        if (tamanio >= arreglo.Length)
+        {
+            Console.WriteLine("El MaxHeap esta lleno.");
+            return;
+        }
+
+        arreglo[tamanio] = libro;
+
+        tamanio++;
+        HeapifyUp(tamanio - 1);
+    }
+
+    // Heapify Up
+    private void HeapifyUp(int indice)
+    {
+        while (indice > 0)
+        {
+            int indicePadre = (indice - 1) / 2;
+            if (arreglo[indice].VecesPrestado > arreglo[indicePadre].VecesPrestado)
+            {
+                Libro temp = arreglo[indice];
+                arreglo[indice] = arreglo[indicePadre];
+                arreglo[indicePadre] = temp;
+
+                indice = indicePadre; 
+            }
+            else
+            {
+                break; 
+            }
+        }
+    }
+
+
+
+    // Obtener top libros más prestados
+    public void MostrarTop(int cantidad)
+    {
+        Console.WriteLine("\n=== TOP " + cantidad + " LIBROS MAS PRESTADOS ===");
+
+        // copia local para no destruir el Heap original
+        MaxHeap copia = new MaxHeap(tamanio);
+        for (int i = 0; i < tamanio; i++)
+        {
+            copia.Insertar(arreglo[i]);
+        }
+
+        // Extraemos la raiz (cantidad) de veces para obtener los libros mas prestados
+        int limite = Math.Min(cantidad, tamanio);
+        for (int i = 0; i < limite; i++)
+        {
+            Libro masPrestado = copia.ExtraerRaiz();
+            if (masPrestado != null)
+            {
+                Console.WriteLine((i + 1) + ". " + masPrestado.Titulo + " (" + masPrestado.VecesPrestado + " prestamos)");
+            }
+        }
+    }
+
+
+
+    // EXTRAER RAIZ (elemento mayor)
+    public Libro ExtraerRaiz()
+    {
+        if (tamanio == 0) return null;
+
+        Libro raiz = arreglo[0];
+
+        arreglo[0] = arreglo[tamanio - 1]; // mover el ultimo a la raiz
+        tamanio--;
+        HeapifyDown(0);
+
+        return raiz;
+    }
+
+
+    // Heapify Down:
+    private void HeapifyDown(int indice)
+    {
+        while (indice < tamanio)
+        {
+            int hijoIzquierdo = (2 * indice) + 1;
+            int hijoDerecho = (2 * indice) + 2;
+            int mayor = indice;
+
+            if (hijoIzquierdo < tamanio && arreglo[hijoIzquierdo].VecesPrestado > arreglo[mayor].VecesPrestado)
+            {
+                mayor = hijoIzquierdo;
+            }
+
+            if (hijoDerecho < tamanio && arreglo[hijoDerecho].VecesPrestado > arreglo[mayor].VecesPrestado)
+            {
+                mayor = hijoDerecho;
+            }
+
+            if (mayor != indice)
+            {
+                Libro temp = arreglo[indice];
+                arreglo[indice] = arreglo[mayor];
+                arreglo[mayor] = temp;
+
+                indice = mayor; 
+            }
+            else
+            {
+                break; 
+            }
+        }
+    }
+}
---
+++ DataStructures/MinHeap.cs
@@ -0,0 +1,129 @@
+//  Min heap
+
+using System;
+
+class MinHeap
+{
+    private Libro[] arreglo;
+    private int tamanio;
+    public MinHeap(int capacidad)
+    {
+        arreglo = new Libro[capacidad];
+        tamanio = 0;
+    }
+
+
+
+    // Insertar nuevo libro
+    public void Insertar(Libro libro)
+    {
+        if (tamanio >= arreglo.Length)
+        {
+            Console.WriteLine("El MinHeap esta lleno.");
+            return;
+        }
+
+        arreglo[tamanio] = libro;
+
+        tamanio++;
+        HeapifyUp(tamanio - 1);
+    }
+
+    // Heapify Up
+    private void HeapifyUp(int indice)
+    {
+        while (indice > 0)
+        {
+            int indicePadre = (indice - 1) / 2;
+            if (arreglo[indice].VecesPrestado < arreglo[indicePadre].VecesPrestado)
+            {
+                Libro temp = arreglo[indice];
+                arreglo[indice] = arreglo[indicePadre];
+                arreglo[indicePadre] = temp;
+
+                indice = indicePadre; 
+            }
+            else
+            {
+                break; 
+            }
+        }
+    }
+
+
+
+    // Obtener top libros menos prestados
+    public void MostrarTop(int cantidad)
+    {
+        Console.WriteLine("\n=== TOP " + cantidad + " LIBROS MENOS PRESTADOS ===");
+
+        // copia local para no destruir el Heap original
+        MinHeap copia = new MinHeap(tamanio);
+        for (int i = 0; i < tamanio; i++)
+        {
+            copia.Insertar(arreglo[i]);
+        }
+
+        // Extraemos la raiz (cantidad) de veces para obtener los libros menos prestados
+        int limite = Math.Min(cantidad, tamanio);
+        for (int i = 0; i < limite; i++)
+        {
+            Libro menosPrestado = copia.ExtraerRaiz();
+            if (menosPrestado != null)
+            {
+                Console.WriteLine((i + 1) + ". " + menosPrestado.Titulo + " (" + menosPrestado.VecesPrestado + " prestamos)");
+            }
+        }
+    }
+
+
+
+    // EXTRAER RAIZ (elemento menor)
+    public Libro ExtraerRaiz()
+    {
+        if (tamanio == 0) return null;
+
+        Libro raiz = arreglo[0];
+
+        arreglo[0] = arreglo[tamanio - 1]; // mover el ultimo a la raiz
+        tamanio--;
+        HeapifyDown(0);
+
+        return raiz;
+    }
+
+
+    // Heapify Down:
+    private void HeapifyDown(int indice)
+    {
+        while (indice < tamanio)
+        {
+            int hijoIzquierdo = (2 * indice) + 1;
+            int hijoDerecho = (2 * indice) + 2;
+            int menor = indice;
+
+            if (hijoIzquierdo < tamanio && arreglo[hijoIzquierdo].VecesPrestado < arreglo[menor].VecesPrestado)
+            {
+                menor = hijoIzquierdo;
+            }
+
+            if (hijoDerecho < tamanio && arreglo[hijoDerecho].VecesPrestado < arreglo[menor].VecesPrestado)
+            {
+                menor = hijoDerecho;
+            }
+
+            if (menor != indice)
+            {
+                Libro temp = arreglo[indice];
+                arreglo[indice] = arreglo[menor];
+                arreglo[menor] = temp;
+
+                indice = menor; 
+            }
+            else
+            {
+                break; 
+            }
+        }
+    }
+}
