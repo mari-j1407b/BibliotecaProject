298e192baf724df84c86ec9af398324bb1c99031
mari-j1407b <mariabelenmg1809@gmail.com>
2026-09-05 23:52:08 -0600

    Pulir estructura nodo b+ (2)

--- DataStructures/NodoBPlus.cs
+++ DataStructures/NodoBPlus.cs
@@ -5,40 +5,21 @@
 // min claves = (m/2)-1 = 1
 // min hijos = m/2 = 2
 
-using System;
-
-class Libro
-{
-    public int Id { get; set; }
-    public string Titulo { get; set; }
-}
-
-class NodoBPlus
-{
-    public bool EsHoja { get; set; }
-    public int NumClaves { get; set; }
-    public Libro[] Claves { get; set; }
-    public NodoBPlus[] Hijos { get; set; }
-    public NodoBPlus Siguiente { get; set; }
-
-    public NodoBPlus(bool esHoja)
-    {
-        EsHoja = esHoja;
-        NumClaves = 0;
-        Claves = new Libro[3];
-        Hijos = new NodoBPlus[4];
-        Siguiente = null;
-    }
-}
-
-class Program
-{
-    static void Main()
-    {
-        NodoBPlus hoja = new NodoBPlus(true);
-        NodoBPlus interno = new NodoBPlus(false);
-
-        Console.WriteLine("Hoja: " + hoja.EsHoja);
-        Console.WriteLine("Interno: " + interno.EsHoja);
-    }
+using System; 
+class NodoBPlus 
+{ 
+    public bool EsHoja { get; set; } 
+    public int NumClaves { get; set; } 
+    public Libro[] Claves { get; set; } // En hojas guarda Libros y nodos internos guarda indices 
+    public NodoBPlus[] Hijos { get; set; } // Apuntadores a nodos hijos (solo nodos internos) 
+    public NodoBPlus Siguiente { get; set; }// Enlace entre hojas
+ 
+    public NodoBPlus(bool esHoja) 
+    { 
+        EsHoja = esHoja; 
+        NumClaves = 0; 
+        Claves = new Libro[3];  // Maximo 3 claves (m - 1) 
+        Hijos = new NodoBPlus[4]; // Maximo 4 hijos (m) 
+        Siguiente = null; 
+    } 
 }
