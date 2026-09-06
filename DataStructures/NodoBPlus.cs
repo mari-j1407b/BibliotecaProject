9933489dfd6f2ffbbc832ba9b6cf56c1a6d9d730
mari-j1407b <mariabelenmg1809@gmail.com>
2026-09-05 23:42:55 -0600

    estrucutura nodo b+

--- DataStructures/NodoBPlus.cs
+++ DataStructures/NodoBPlus.cs
@@ -1,6 +1,23 @@
 using System;
 
+class Libro
+{
+    public int Id { get; set; }
+    public string Titulo { get; set; }
+}
+
 class NodoBPlus
 {
-    
+    public bool EsHoja { get; set; }
+    public Libro[] Claves { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        NodoBPlus nodo = new NodoBPlus();
+
+        Console.WriteLine("Nodo creado");
+    }
 }
