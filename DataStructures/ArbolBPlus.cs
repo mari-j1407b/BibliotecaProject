62e5e1d1e719355537373eb2322f38e5b73c5330
mari-j1407b <mariabelenmg1809@gmail.com>
2026-09-06 00:30:37 -0600

    implementar busqueda de libros

--- DataStructures/ArbolBPlus.cs
+++ DataStructures/ArbolBPlus.cs
@@ -9,4 +9,34 @@ class ArbolBPlus
     {
         raiz = new NodoBPlus(true);
     }
+
+// Buscar libro por su codigo
+    public Libro Buscar(string codigo)
+    {
+        return BuscarEnNodo(raiz, codigo);
+    }
+
+    private Libro BuscarEnNodo(NodoBPlus nodo, string codigo)
+    {
+        int i = 0;
+
+        while (i < nodo.NumClaves &&
+               string.Compare(codigo, nodo.Claves[i].Codigo) > 0)
+        {
+            i++;
+        }
+
+        if (nodo.EsHoja)
+        {
+            if (i < nodo.NumClaves &&
+                nodo.Claves[i].Codigo == codigo)
+            {
+                return nodo.Claves[i];
+            }
+
+            return null;
+        }
+
+        return BuscarEnNodo(nodo.Hijos[i], codigo);
+    }
 }
